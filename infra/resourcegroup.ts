23ec14e87dc7d3e0ff9185b89db0fa8d89e2ac33
Cory Leclair <cory.leclair@gmail.com>
2022-05-11 14:44:08 -0400

    added location to resource group

--- infra/resourcegroup.ts
+++ infra/resourcegroup.ts
@@ -1,4 +1,4 @@
 import * as resources from "@pulumi/azure-native/resources";
 import * as pulumi from "@pulumi/pulumi";
 
-export const resourceGroup = new resources.ResourceGroup("clrg-");
+export const resourceGroup = new resources.ResourceGroup("clrg-", {location: "canadacentral"});
